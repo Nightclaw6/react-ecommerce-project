bc42efc9d00c02e76d0b45554c4d345e032f9cd6
Nightclaw6 <sclaudiu82@yahoo.com>
2023-01-20 14:22:43 -0500

    refactored authentication section

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,14 @@
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
-
+import { UserContext } from '../../contexts/user.context';
+import { signOutUser } from '../../utils/firebase/firebase.utils';
 import './navigation.styles.css';
 
 const Navigation = () => {
+  
+  const { currentUser } = useContext(UserContext);
+  
   return (
     <Fragment>
       <div className="navigation">
@@ -15,9 +19,13 @@ const Navigation = () => {
             <Link className="nav-link" to="/shop">
               Shop
             </Link>
-            <Link className="nav-link" to="/auth">
+            { currentUser ? (
+              <span className="nav-link" onClick={signOutUser}>Sign Out</span>)
+              : (<Link className="nav-link" to="/auth">
               Sign In
-            </Link>
+            </Link>)
+            }
+            
         </div>
       </div>
       <Outlet />
